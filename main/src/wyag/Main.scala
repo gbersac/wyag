45ec90c2abff53e4923f6439885ae3babd980d69
Guillaume Bersac <bersac_1@hotmail.fr>
2019-07-07 13:58:49 +0200

    Main expect a string and print result

--- main/src/wyag/Main.scala
+++ main/src/wyag/Main.scala
@@ -2,29 +2,28 @@ import ammonite.ops._
 import scala.util.Try
 
 object CLI {
-  def argsParse(args: Seq[String]): Either[WyagError, Unit] = {
+  def argsParse(args: Seq[String]): Either[WyagError, String] = {
     args.toList match {
       case subCommand :: tail =>
         subCommand match {
           case "init" =>
             val path: Either[WyagError, Path] = tail.lift(0).map(x => WyagError.tc(Path(x))).getOrElse(Right(pwd))
-            path.flatMap(p => GitRepository.createRepo(p)).map(_ => ())
+            path.flatMap(p => GitRepository.createRepo(p)).map(_ => "Successfully initiated repo")
           case "cat-file" =>
             for {
               repo <- GitRepository.findRepo()
 
-              _ <- {
+              content <- {
                 tail match {
                   case GitObjectType(typ) :: objName :: Nil =>
                     val obj = repo.findObject(objName)
-                    obj.map(o => println(o.content))
-                    obj.map(_ => ())
+                    obj.map(o => o.content)
                   case _ =>
                     Left(WyagError(s"Should be wyag / object type / object name"))
                 }
               }
 
-            } yield ()
+            } yield content.toString
           case _ =>  WyagError.l(s"Unknown command $subCommand")
         }
       case _ => WyagError.l("Usage `wyag subCommand`")
@@ -36,6 +35,9 @@ object CLI {
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Result:", CLI.argsParse(args))
+    CLI.argsParse(args) match {
+      case Left(err) => println("Error:", err.msg)
+      case Right(v) => println(v)
+    }
   }
 }
