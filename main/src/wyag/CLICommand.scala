fd747a390045c8b01ae5895b8a456fac43bd413e
Guillaume Bersac <bersac_1@hotmail.fr>
2019-07-07 15:10:19 +0200

    isolate command line arguments parsing

---
+++ main/src/wyag/CLICommand.scala
@@ -0,0 +1,53 @@
+import ammonite.ops._
+import scala.util.Try
+
+/**
+ * CLI for command line interface
+ */
+sealed trait CLICommand
+
+object CLICommand {
+
+  case class Init(p: Option[Path]) extends CLICommand
+  case class CatFile(typ: GitObjectType, name: String) extends CLICommand
+  case class HashObject(file: Path, typ: Option[GitObjectType], write: Boolean) extends CLICommand
+
+  def argsParse(args: Seq[String]): Either[WyagError, CLICommand] = {
+    args.toList match {
+      case subCommand :: tail =>
+        subCommand match {
+
+          case "init" =>
+            tail match {
+              case PathS(p) :: Nil => Right(Init(Some(p)))
+              case Nil => Right(Init(None))
+              case _ => WyagError.l("")
+            }
+
+          case "cat-file" =>
+            tail match {
+              case GitObjectType(typ) :: objName :: Nil => Right(CatFile(typ, objName))
+              case _ => WyagError.l("")
+            }
+
+          case "hash-object" =>
+            tail match {
+              case PathS(p) :: Nil => Right(HashObject(p, None, false))
+              case _ => WyagError.l("")
+            }
+
+          case _ =>  WyagError.l(s"Unknown command $subCommand")
+        }
+      case _ => WyagError.l("Usage `wyag subCommand`")
+    }
+  }
+
+  def optionValue(args: Seq[String], value: String): Option[String] =
+    if (args.contains(value)) args.lift(args.indexOf(value) + 1)
+    else None
+
+  object PathS {
+    def unapply(s: String): Option[Path] = Try(Path(s)).toOption
+  }
+
+}
--- main/src/wyag/Main.scala
+++ main/src/wyag/Main.scala
@@ -1,41 +1,31 @@
 import ammonite.ops._
-import scala.util.Try
 
-object CLI {
-  def argsParse(args: Seq[String]): Either[WyagError, String] = {
-    args.toList match {
-      case subCommand :: tail =>
-        subCommand match {
-          case "init" =>
-            val path: Either[WyagError, Path] = tail.lift(0).map(x => WyagError.tc(Path(x))).getOrElse(Right(pwd))
-            path.flatMap(p => GitRepository.createRepo(p)).map(_ => "Successfully initiated repo")
-          case "cat-file" =>
-            for {
-              repo <- GitRepository.findRepo()
+object Executor {
+  def execute(command: CLICommand): Either[WyagError, String] = command match {
+    case CLICommand.Init(p) =>
+      val path = p.getOrElse(pwd)
+      GitRepository.createRepo(path).map(_ => "Successfully initiated repo")
+    case CLICommand.CatFile(typ: GitObjectType, name: String) =>
+      for {
+        repo <- GitRepository.findRepo()
 
-              content <- {
-                tail match {
-                  case GitObjectType(typ) :: objName :: Nil =>
-                    val obj = repo.findObject(objName)
-                    obj.map(o => o.content)
-                  case _ =>
-                    Left(WyagError(s"Should be wyag / object type / object name"))
-                }
-              }
-
-            } yield content.toString
-          case _ =>  WyagError.l(s"Unknown command $subCommand")
+        content <- {
+          val obj = repo.findObject(name)
+          obj.map(o => o.content)
         }
-      case _ => WyagError.l("Usage `wyag subCommand`")
-    }
+
+      } yield content.toString
+
+    case CLICommand.HashObject(file: Path, typ: Option[GitObjectType], write: Boolean) =>
+      ???
   }
 
-  def findRepo: Either[WyagError, GitRepository] = GitRepository.findRepo()
+  private def findRepo: Either[WyagError, GitRepository] = GitRepository.findRepo()
 }
 
 object Main {
   def main(args: Array[String]): Unit = {
-    CLI.argsParse(args) match {
+    CLICommand.argsParse(args).flatMap(Executor.execute) match {
       case Left(err) => println("Error:", err.msg)
       case Right(v) => println(v)
     }
